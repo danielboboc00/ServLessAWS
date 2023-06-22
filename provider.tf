43ab1b78d46959de82eb97f7e1d1737b3ce8312d
danielboboc00 <danielboboc00@gmail.com>
2023-06-22 10:10:30 +0300

    created a bucket + provider

---
+++ provider.tf
@@ -0,0 +1,18 @@
+// Specificam versiunile pentru providerii folositi
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.4"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.1"
+    }
+  }
+}
+
+#Region settings
+provider "aws" {
+  region = "us-east-1"
+}
